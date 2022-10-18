203059d545f9855d4fa001299316a87f5bc4455e
SARB <mrsinghbusiness05@gmail.com>
2022-10-18 22:16:45 +0530

    About Part dON===DE

---
+++ src/About.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+ function About() {
+  return (
+    <div name = "about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className = "max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className='text-4xl font-bold inline-border-b-4 border-gray-500' >
+                        About 
+                    </p>
+                </div>
+
+                <p className='text-xl mt-20'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugit ipsum voluptatum dolor cumque aspernatur, esse 
+            aut harum odio eum, quae ea vitae necessitatibus dolorum
+             suscipit fuga minima officia, in velit commodi eos
+              amet ratione ad eius sapiente? Totam architecto, 
+              ab iusto obcaecati quis dignissimos praesentium!
+         </p>
+         <br />
+         <p className='text-xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptatibus esse amet suscipit doloribus eius minus illo,
+             voluptas minima, assumenda quasi hic quam doloremque 
+             repellendus magnam architecto obcaecati repudiandae quae 
+             fugiat cumque nemo ab exercitationem nulla optio. Corrupti
+              minus facilis numquam sed quidem voluptatibus qui earum.
+         </p>
+
+
+
+        </div>
+
+        
+    </div>
+  )
+}
+export default About 
--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import SocialLinks from './SocialLinks';
+import About from './About';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
        <h2> 
            <Navbar /> 
            <Home />
+           <SocialLinks />
+           <About />
        </h2>
     </div>
   );
